b281bad1fd1e5153e364e93dac9fc60a4819ef33
Everton Alves da Silva <everton.silva@wooza.com.br>
2019-08-29 14:13:19 -0300

    Inclusão de Testes de Integração e Unitario

---
+++ tests/Actio.Services.Identity.Tests/Integration/Controllers/AccountControllerTests.cs
@@ -0,0 +1,48 @@
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Actio.Common.Auth;
+using FluentAssertions;
+
+namespace Actio.Services.Identity.Tests.Integration.Controllers
+{
+    public class AccountControllerTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+        
+        public AccountControllerTests()
+        {
+            _server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task account_controller_login_should_return_json_web_token()
+        {
+            var payload = GetPayload(new {email = "user1@email.com", password = "secret"});
+            var response = await _client.PostAsync("login", payload);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var jwt = JsonConvert.DeserializeObject<JsonWebToken>(content);
+
+            jwt.Should().NotBeNull();
+            jwt.Token.Should().NotBeEmpty();
+            jwt.Expires.Should().BeGreaterThan(0);
+        }
+
+        protected static StringContent GetPayload(object data)
+        {
+            var json = JsonConvert.SerializeObject(data);
+
+            return new StringContent(json, Encoding.UTF8, "application/json");
+        }
+    }
+}
